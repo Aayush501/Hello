9d6353ea5ffcded8f1c1d382ee91be0d1c94d2f1
Aayush501 <shrivastavashu501@gmail.com>
2025-01-07 01:12:07 +0530

    added files

---
+++ Sub.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+
+public class Sub{
+    public static void main(){
+        Scanner sc = new Scanner(System.in);
+        int k = sc.nextInt();
+        int l = sc.nextInt();
+        System.out.println(k-l);
+    }
+}
